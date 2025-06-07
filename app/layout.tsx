73e8e57240d66e99067f19835a3f2a62a314a736
Sharadamani K N <sharadamanikn@gmail.com>
2025-06-07 19:42:44 +0530

    feat: remove old scrollbar styles and update layout for improved UI consistency

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,15 @@
 import "./globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
+import { PropsWithChildren } from "react";
 
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-<<<<<<< HEAD
   title: "SecondBrain",
 };
 
 const RootLayout = ({ children }: PropsWithChildren) => {
-=======
-  title: "MemoryVault",
-}
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
->>>>>>> 912293569f050667b3eebaeed63b3404fe683fd9
   return (
     <html lang="en">
       <body className={inter.className}>
